76fd69cbca6d855b5851516f5d0ddba25465a68a
warned101 <astitvagupta123@gmail.com>
2018-10-05 11:06:12 +0530

    second commit

---
+++ app/src/main/java/com/example/arkoz/paytmintegration/Api.java
@@ -0,0 +1,28 @@
+package com.example.arkoz.paytmintegration;
+
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface Api {
+
+    //this is the URL of the paytm folder that we added in the server
+    //make sure you are using your ip else it will not work
+    String BASE_URL = "http://192.168.43.109/paytm/";
+
+    @FormUrlEncoded
+    @POST("generateChecksum.php")
+    Call<Checksum> getChecksum(
+            @Field("MID") String mId,
+            @Field("ORDER_ID") String orderId,
+            @Field("CUST_ID") String custId,
+            @Field("CHANNEL_ID") String channelId,
+            @Field("TXN_AMOUNT") String txnAmount,
+            @Field("WEBSITE") String website,
+            @Field("CALLBACK_URL") String callbackUrl,
+            @Field("INDUSTRY_TYPE_ID") String industryTypeId
+    );
+
+}
---
+++ app/src/main/java/com/example/arkoz/paytmintegration/Constants.java
@@ -0,0 +1,11 @@
+package com.example.arkoz.paytmintegration;
+
+public class Constants {
+    public static final String M_ID = "Ozimne22727132127010"; //Paytm Merchand Id we got it in paytm credentials
+    public static final String CHANNEL_ID = "WAP"; //Paytm Channel Id, got it in paytm credentials
+    public static final String INDUSTRY_TYPE_ID = "Retail"; //Paytm industry type got it in paytm credential
+
+    public static final String WEBSITE = "APP_STAGING";
+    public static final String CALLBACK_URL = "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp";
+
+}
